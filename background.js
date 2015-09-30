667c8d01ba65675dabe3c9e47e52f8ba62592529
blackrabbit <myzlio@gmail.com>
2015-09-30 13:28:09 +0300

    add phrases

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 (function (chrome) {
   var PageObserver = function(){
     var options = {
-      url: 'http://www.thefreedictionary.com/'
+      url: {
+        word: 'http://www.thefreedictionary.com/',
+        idiom: 'http://idioms.thefreedictionary.com/'
+      }
     };
     var bindEvents = function(){
         chrome.runtime.onInstalled.addListener(attachContextMenu);
@@ -14,16 +17,25 @@
       var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context});
     };
 
+    var getUrl = function(text){
+      var isWord = text.split(' ').length > 1;
+
+      var url = isWord ? options.url.word : options.url.idiom;
+
+      url+= isWord ? text : text.split(' ').join('+');
+
+      return url;
+    };
+
     var openPopup = function(info, tab){
       var sText = info.selectionText;
       chrome.windows.create({
-        'url': options.url + sText,
+        'url': getUrl(sText),
         'type': 'popup',
         width: 500,
         height: 700,
       }, Function.prototype);
     };
-
 
     bindEvents();
 
@@ -31,30 +43,3 @@
 
   pageObserver = new PageObserver();
 })(chrome);
-// (function(){
-//   chrome.runtime.onInstalled.addListener(function() {
-//     var context = "selection";
-//     var title = "Google for Selected Text";
-//     var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-//                                            "id": "context" + context});
-//   });
-//
-// })()
-//
-//
-//
-//
-//
-// // add click event
-// chrome.contextMenus.onClicked.addListener(onClickHandler);
-//
-// // The onClicked callback function.
-// function onClickHandler(info, tab) {
-//   var sText = info.selectionText;
-//   chrome.windows.create({
-//     'url': 'http://www.thefreedictionary.com/' + sText,
-//     'type': 'popup',
-//     width: 500,
-//     height: 700,
-//   }, function(window) {});
-// };
